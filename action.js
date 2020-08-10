7dee38d42fde05049de920273050cc1037cd1f63
coolchandan <69463218+coolchandan@users.noreply.github.com>
2020-08-10 22:02:35 +0530

    Delete action.js

--- action.js
+++
@@ -1,9 +0,0 @@
-
-import * as actionTypes from '../store/actionTypes';
-
-export const loginAction = (data) => {
-    return dispatch => {
-            dispatch({type:actionTypes.IS_AUTH,data });
-       
-    };
-};
